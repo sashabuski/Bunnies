a09f07650e797ef4dd1b61cd18108dd24d1bdeb4
JianyiMao <89119894+JianyiMao@users.noreply.github.com>
2021-10-15 12:01:04 +0800

    Add files via upload

---
+++ HopIn/src/main/java/com/HopIn/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.hopln;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView welcomeImg = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        welcomeImg = (ImageView) this.findViewById(R.id.welcome_img);
+        AlphaAnimation anima = new AlphaAnimation(0.3f, 1.0f);
+        anima.setDuration(3000);
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new AnimationImpl());
+    }
+
+    private class AnimationImpl implements Animation.AnimationListener
+    {
+
+        @Override
+        public void onAnimationStart(Animation animation)
+        {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation)
+        {
+            skip();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation)
+        {
+
+        }
+
+    }
+
+    private void skip()
+    {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
