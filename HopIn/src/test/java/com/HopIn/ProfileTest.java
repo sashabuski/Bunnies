f1f0581750a64e4abcdc7ba8aa53057421d99d8e
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2021-10-26 23:07:14 +1300

    Updated ProfileTest.java via browser
    Minor tweak to update name and remove syntax errors

--- HopIn/src/test/java/com/HopIn/ProfileTest.java
+++ HopIn/src/test/java/com/HopIn/ProfileTest.java
@@ -2,26 +2,23 @@ package com.HopIn;
 
 import junit.framework.TestCase;
 
-
+/**
+ * This test exhibits the functionality of the updateUser() of the Profile Class.
+ * The user is allowed to update their name via the Profile page. However, leaving an empty firstname should not be allowed.
+ * The purpose of this test is to pass two Strings, one with alphabets and another with spaces.
+ * Without alphabets
+ *
+ * This test asserts false if only spaces or empty.
+ *
+ */
 public class ProfileTest extends TestCase {
 
-
-    /**
-     * This test exhibits the functionality of the updateUser() of the Profile Class.
-     * The user is allowed to update their name via the Profile page. However, leaving an empty firstname should not be allowed.
-     * The purpose of this test is to pass two Strings, one with alphabets and another with spaces.
-     * Without alphabets 
-     *
-     * This test asserts false if only spaces or empty.
-     *
-     */
-
-    public void testProfile() {
+    public void testisNameValid() {
 
         Profile profile = new Profile();
         
-		firstname="Testing"
-		secondname=" "
+		String firstname="Testing";
+		String secondname=" ";
 
         assertTrue(profile.isNameValid(firstname));
 		assertFalse(profile.isNameValid(secondname));
